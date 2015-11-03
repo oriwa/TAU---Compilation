1e640d98fecb0fd7a535b81f304056c0db339d8b
Ori Wasserman <oriwass@gmail.com>
2015-11-04 00:10:36 +0200

    removed lexer

--- LEXER/jflex-1.6.1/bin/jflex.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Please adjust JFLEX_HOME to suit your needs
-REM (please do not add a trailing backslash)
-
-set JFLEX_HOME=D:\Ori\Downloads\LEXER\LEXER\jflex-1.6.1
-
-java -Xmx128m -jar %JFLEX_HOME%\lib\jflex-1.6.1.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
