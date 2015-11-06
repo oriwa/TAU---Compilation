6b4e898f2de7de61275efa5b567a8e90a345ced4
Ori Wasserman <oriwass@gmail.com>
2015-11-06 16:06:28 +0200

    added jflex, .project and cup

--- LEXER/src/sym.java
+++ LEXER/src/sym.java
@@ -7,15 +7,55 @@
 /** CUP generated interface containing symbol constants. */
 public interface sym {
   /* terminals */
-  public static final int DIVIDE = 5;
-  public static final int NUMBER = 8;
-  public static final int EOF = 0;
-  public static final int PLUS = 3;
-  public static final int ID = 9;
-  public static final int error = 1;
-  public static final int SEMICOLON = 2;
-  public static final int RPAREN = 7;
-  public static final int TIMES = 4;
-  public static final int LPAREN = 6;
+
+	public static final int EOF = 0;
+	public static final int ASSIGN = 1;
+	public static final int BOOLEAN = 2;
+	public static final int CLASS = 3;
+	public static final int CLASS_ID = 4;
+	public static final int EXTENDS = 5;
+	public static final int RETURN = 6;
+	public static final int SEMI = 7;
+	public static final int COMMA = 8;
+	public static final int STATIC = 9;
+	public static final int STRING = 10;
+	public static final int QUOTE = 11;
+	public static final int THIS = 12;
+	public static final int VOID = 13;
+	public static final int CONTINUE = 14;
+	public static final int BREAK = 15;
+	public static final int WHILE = 16;
+	public static final int DOT = 17;
+	public static final int IF = 18;
+	public static final int ELSE = 19;
+	public static final int TRUE = 20;
+	public static final int FALSE = 21;
+	public static final int ID = 22;
+	public static final int NULL = 23;
+	public static final int GT = 24;
+	public static final int GTE = 25;
+	public static final int LT = 26;
+	public static final int LTE = 27;
+	public static final int INT = 28;
+	public static final int INTEGER = 29;
+	public static final int LENGTH = 30;
+	public static final int NEW = 31;
+	public static final int LNEG = 32;
+	public static final int LAND = 33;
+	public static final int LOR = 34;
+	public static final int EQUAL = 35;
+	public static final int NEQUAL = 36;
+	public static final int MINUS = 37;
+	public static final int MOD = 38;
+	public static final int PLUS = 39;
+	public static final int MULTIPY = 40;
+	public static final int DIVIDE = 41;
+	public static final int LB = 42;
+	public static final int RB = 43;
+	public static final int LP = 44;
+	public static final int RP = 45;
+	public static final int LCBR = 46;
+	public static final int RCBR = 47;
+
 }
 
