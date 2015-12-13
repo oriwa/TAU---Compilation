c384546f05b59cd3185c51d5c6e011b018fbf607
Ori Wasserman <oriwass@gmail.com>
2015-12-13 03:16:19 +0200

    actual bugfix

--- slp/src/slp/Environment.java
+++ slp/src/slp/Environment.java
@@ -203,8 +203,8 @@ public class Environment {
 				
 			}
 			else
-			{					
-				handleSemanticError("class \""+clss.extends_class.name +"\" is undefined, classes can only extend previously defined classes",clss.line);
+			{
+				handleSemanticError("class \""+clss.extends_name +"\" is undefined, classes can only extend previously defined classes",clss.line);
 			}
 		}
 		
